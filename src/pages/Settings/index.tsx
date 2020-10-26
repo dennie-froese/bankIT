641516c3a90a066fc33d2023c8902f37ffb0b38c
Dennie Froese <dennie.froese@santander.co.uk>
2020-10-26 19:58:22 +0000

    added settings page placeholder to authorised journey

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {colours, spacing, borderRadii} from '../../constants';
+import {
+  useStateMachineState,
+  useStateMachineDispatch,
+} from '../../state/StateMachine';
+import PrimaryButton from '../../components/PrimaryButton';
+
+export default function Settings() {
+  const current = useStateMachineState();
+  const send = useStateMachineDispatch();
+  const {user, password} = current?.context;
+
+  const goToAccount = () => {
+    send('DONE');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.textHeader}>Settings!</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  textHeader: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
