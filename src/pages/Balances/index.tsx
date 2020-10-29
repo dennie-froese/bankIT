f9ddabbcdc93ed6ddc617f81ac040b544fe8eba4
Dennie Froese <dennie.froese@santander.co.uk>
2020-10-29 21:17:37 +0000

    added content to balances page

--- src/pages/Balances/index.tsx
+++ src/pages/Balances/index.tsx
@@ -1,43 +1,61 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {colours, spacing, borderRadii} from '../../constants';
-import {
-  useStateMachineState,
-  useStateMachineDispatch,
-} from '../../state/StateMachine';
-import PrimaryButton from '../../components/PrimaryButton';
+import 'react-native-gesture-handler';
+import React, {useCallback} from 'react';
+import {StyleSheet, View, Text, BackHandler, Alert} from 'react-native';
+import Account from '../../components/Account';
+import {useFocusEffect} from '@react-navigation/core';
 
 export default function Balances() {
-  const current = useStateMachineState();
-  const send = useStateMachineDispatch();
-  const {user, password} = current?.context;
+  useFocusEffect(
+    useCallback(() => {
+      const onBackPress = () => {
+        Alert.alert(
+          'Exit App',
+          'Exiting the application?',
+          [
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel',
+            },
+            {
+              text: 'OK',
+              onPress: () => BackHandler.exitApp(),
+            },
+          ],
+          {
+            cancelable: false,
+          },
+        );
+        return true;
+      };
 
-  const goToAccount = () => {
-    send('DONE');
-  };
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
 
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, []),
+  );
   return (
-    <View style={styles.container}>
-      <View style={styles.topContainer}>
-        <Text style={styles.textHeader}>BLABALA!</Text>
+    <View>
+      <Text style={styles.text}>Please find your account balances below:</Text>
+      <View style={styles.baseContainer}>
+        <View style={styles.accountContainer}>
+          <Account accountType={'Current Account'} />
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  topContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  textHeader: {
+  text: {
     textAlign: 'center',
+    paddingTop: 10,
+  },
+  baseContainer: {
     padding: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
+  },
+  accountContainer: {
+    backgroundColor: 'white',
   },
 });
